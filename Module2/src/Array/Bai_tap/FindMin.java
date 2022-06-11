7eeef4e3b574c4dcdb72b45b3e618c1d72fbbe33
anhhtgcd191331 <80952816+anhhtgcd191331@users.noreply.github.com>
2022-06-11 16:41:38 +0700

    á

--- Module2/src/Array/Bai_tap/FindMin.java
+++ Module2/src/Array/Bai_tap/FindMin.java
@@ -1,1 +1,24 @@
-s
+package Array.Bai_tap;
+
+import java.util.Scanner;
+
+public class FindMin {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int size = input.nextInt();
+        int[] array = new int[size];
+        for (int i= 0; i<array.length; i++){
+            System.out.printf("array[%d] = ",i);
+            int value = input.nextInt();
+            array[i] = value;
+        }
+
+        int min = array[0];
+        for(int i = 0; i<array.length; i++){
+            if(array[i]<min){
+                min = array[i];
+            }
+        }
+        System.out.println("Min in array: " + min);
+    }
+}
