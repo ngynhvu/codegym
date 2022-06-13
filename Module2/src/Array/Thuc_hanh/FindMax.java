1f6cdab84dbd5c2d70158d49dc1a527d02a75043
anhhtgcd191331 <80952816+anhhtgcd191331@users.noreply.github.com>
2022-06-13 18:01:31 +0700

    zx

--- Module2/src/Array/Thuc_hanh/FindMax.java
+++ Module2/src/Array/Thuc_hanh/FindMax.java
@@ -1,2 +1,34 @@
-package Array.Thuc_hanh;public class FindMax {
+package Array.Thuc_hanh;
+
+import java.util.Scanner;
+
+public class FindMax {
+    public static void main(String[] args) {
+        int size;
+        Scanner scanner = new Scanner(System.in);
+        do {
+            System.out.print("Enter a size:");
+            size = scanner.nextInt();
+            if (size > 20)
+                System.out.println("Size should not exceed 20");
+        } while (size > 20);
+        int[] array = new int[size];
+        for (int i = 0; i < array.length; i++) {
+            System.out.print("Enter element" + (i + 1) + " : ");
+            array[i] = scanner.nextInt();
+        }
+        System.out.print("Property list: ");
+        for (int j = 0; j < array.length; j++) {
+            System.out.print(array[j] + "\t");
+        }
+        int max = array[0];
+        int location = 0;
+        for (int i = 0; i < array.length; i++) {
+            if(array[i]>max){
+                max =array[i];
+                location = i;
+            }
+        }
+        System.out.println("The largest property value in the list is " + max + " ,at position " + location);
+    }
 }
