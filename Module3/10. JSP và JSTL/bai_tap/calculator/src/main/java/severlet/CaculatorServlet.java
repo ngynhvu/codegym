e39d1b0fd3d5ee1a05d26990c58912c1b6daf3ec
anhhtgcd191331 <80952816+anhhtgcd191331@users.noreply.github.com>
2022-09-16 19:49:07 +0700

    sd

---
+++ Module3/10. JSP và JSTL/bai_tap/calculator/src/main/java/severlet/CaculatorServlet.java
@@ -0,0 +1,29 @@
+package severlet;
+
+import model.Caculator;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@javax.servlet.annotation.WebServlet(name = "CaculatorServlet", urlPatterns = "/calculate")
+public class CaculatorServlet extends javax.servlet.http.HttpServlet {
+    protected void doPost(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
+        float firstOperand = Integer.parseInt(request.getParameter("first-operand"));
+        float secondOperand = Integer.parseInt(request.getParameter("second-operand"));
+        char operator = request.getParameter("operator").charAt(0);
+        PrintWriter writer = response.getWriter();
+        writer.println("<html>");
+        writer.println("<h1>Result:</h1>");
+        try{
+            float result = Caculator.calculate(firstOperand, secondOperand, operator);
+            writer.println(firstOperand + " " + operator + " " + secondOperand + " = " + result);
+        }catch (Exception ex){
+            writer.println("Error: " + ex.getMessage());
+        }
+        writer.println("</html>");
+    }
+
+    protected void doGet(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
+
+    }
+}
