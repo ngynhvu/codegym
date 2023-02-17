dc4e7806dc478f510f8d00eed02706bed74237e5
anhhtgcd191331 <anhhtgcd191331@fpt.edu.vn>
2023-02-17 18:00:59 +0700

    ok

---
+++ Module5/ss1_UX_UI/Program.cs
@@ -0,0 +1,13 @@
+﻿
+DateTime d1 = DateTime.Now;
+DateTime d2 = new DateTime(2023,2,16);
+int result = DateTime.Compare(d1,d2);
+string relationship;
+if (result < 0)
+   relationship = "is earlier than";
+else if (result == 0)
+   relationship = "is the same time as";         
+else
+   relationship = "is later than";
+ 
+Console.WriteLine("{0} {1} {2}", d1, relationship, d2);
